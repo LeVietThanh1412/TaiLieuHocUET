879fd0f5129e266f12a95fd20af26d72c01d6502
Lê Việt Thành <24022454@vnu.edu.vn>
2025-06-30 23:54:16 +0700

    Update Tài Liệu 30/6/2025

---
+++ 1 Ôn DSA/test.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+
+#define vec1(type_data) vector<type_data>
+#define cd(x) fixed << setprecision(x)
+#define pr(x) cout << x
+
+const ll mod = 1e9 + 7;
+const double pi = 3.1415;
+
+#ifdef LOCAL
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+#else
+#define dbg(...)
+#endif
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    pr("Hello");
+    return 0;
+}
